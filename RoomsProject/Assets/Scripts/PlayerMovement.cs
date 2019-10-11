5b7ae740a63505f22f883fb704976313f05fd738
PatrickIckes <43104857+PatrickIckes@users.noreply.github.com>
2019-10-11 11:33:48 -0500

    door scene change

--- RoomsProject/Assets/Scripts/PlayerMovement.cs
+++ RoomsProject/Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 1f;
+    public float jumpSpeed = 1f;
+    public bool withinInteractable;
     //IsometricCharacterRenderer isoRenderer;
 
     Rigidbody2D rbody;
+    Collider2D colliderTrigger;
 
     //Used to track when items are picked up
     public Inventory inventory;
@@ -15,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         rbody = GetComponent<Rigidbody2D>();
+        colliderTrigger = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -23,11 +28,20 @@ public class PlayerMovement : MonoBehaviour
         Vector2 currentPos = rbody.position;
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        float jumpInput = Input.GetAxis("Jump");
         Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
         inputVector = Vector2.ClampMagnitude(inputVector, 1);
         Vector2 movement = inputVector * movementSpeed;
         Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
         rbody.MovePosition(newPos);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && withinInteractable)
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 
     //private void OnControllerColliderHit(ControllerColliderHit hit)
@@ -39,12 +53,30 @@ public class PlayerMovement : MonoBehaviour
     //    }
     //}
 
-    private void OnTriggerEnter(Collider other)
+    //private void CheckForEntrance()
+    //{
+
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         IInventoryItem item = GetComponent<IInventoryItem>();
         if (item != null)
         {
             inventory.AddItem(item);
+        }
+
+        if(collision.name == "Door")
+        {
+            withinInteractable = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "Door")
+        {
+            withinInteractable = false;
         }
     }
 }
