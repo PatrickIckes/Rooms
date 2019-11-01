6f2944dd80ee16ee4e598dff8786364d09eeb625
PatrickIckes <43104857+PatrickIckes@users.noreply.github.com>
2019-11-01 10:34:38 -0500

    rough main menu added

---
+++ RoomsProject/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Button StartGameButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGameButton.onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
