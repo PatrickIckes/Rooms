675aab59efa7b65075d045d0be975254c4181fa3
PatrickIckes <patrick.ickes@loop.colum.edu>
2019-11-22 01:12:11 -0600

    Various small changes

--- RoomsProject/Assets/Scripts/MainMenuController.cs
+++ RoomsProject/Assets/Scripts/MainMenuController.cs
@@ -7,18 +7,21 @@ using UnityEngine.UI;
 public class MainMenuController : MonoBehaviour
 {
     public Button StartGameButton;
+    public Button PlayGameButton;
     public Button SettingsButton;
     public Button CreditsButton;
     public Button CreditsBackButton;
     public Button SettingsBackButton;
     public GameObject MainMenuCanvas;
+    public GameObject StoryCanvas;
     public GameObject SettingsCanvas;
     public GameObject CreditsCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartGameButton.onClick.AddListener(StartGame);
+        StartGameButton.onClick.AddListener(Story);
+        PlayGameButton.onClick.AddListener(StartGame);
         SettingsButton.onClick.AddListener(Settings);
         CreditsButton.onClick.AddListener(Credits);
         CreditsBackButton.onClick.AddListener(CreditsBack);
@@ -28,6 +31,12 @@ public class MainMenuController : MonoBehaviour
     void StartGame()
     {
         SceneManager.LoadScene(0);
+    }
+
+    void Story()
+    {
+        MainMenuCanvas.SetActive(false);
+        StoryCanvas.SetActive(true);
     }
 
     void Credits()
