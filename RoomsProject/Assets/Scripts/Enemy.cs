b028ef0aa476740a86ce7aff4456017842373eb8
RickyBobby <55592577+rmiranda6@users.noreply.github.com>
2019-11-21 18:27:23 -0600

    Added Player Attacking and Enemy Health
    Better player attacking script, and added enemy health.

---
+++ RoomsProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int enemyHealth;
+    public GameObject bloodEffect;
+    void Start()
+    {
+        //Add animation
+    }
+
+    void Update()
+    {
+        if(enemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //Play hurt sound of enemy here
+        Instantiate(bloodEffect, transform.position, Quaternion.identity);
+        enemyHealth -= damage;
+        Debug.Log("Damage TAKEN!");
+    }
+}
---
+++ RoomsProject/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRangeX;
+    public float attackRangeY;
+    public int damage;
+
+    void Update()
+    {
+        if(timeBtwAttack <= 0)
+        {
+            //Then you can attack
+            if(Input.GetKey(KeyCode.F))
+            {
+                //Player attacking animation here
+                Collider2D[] enemiesToDamage = Physics2D.OverlapBoxAll(attackPos.position, new Vector2(attackRangeX, attackRangeY), 0, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+            timeBtwAttack = startTimeBtwAttack;
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(attackPos.position, new Vector3(attackRangeX, attackRangeY, 1));
+    }
+
+}
