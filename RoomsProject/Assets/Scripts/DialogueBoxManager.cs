06b53a6d2afb7b27e6c921edc287843d75455b83
AYogore <AYogore@users.noreply.github.com>
2020-02-19 09:11:02 -0600

    dialoogue

---
+++ RoomsProject/Assets/Scripts/DialogueBoxManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueBoxManager : MonoBehaviour
+{
+    public GameObject textBox;
+
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+
+
+
+    //add pause from inventory
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //imports txt file into Dialogue Manager
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        theText.text = textLines[currentLine];
+        //start of dialogue,f to continue
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            currentLine += 1;
+
+        }
+
+        if (currentLine == endAtLine)
+        {
+            textBox.SetActive(false);
+        }
+    }
+}
---
+++ RoomsProject/Assets/Scripts/DialogueImporter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DialogueImporter : MonoBehaviour
+{
+    [SerializeField] private bool trigger = false;
+    [SerializeField] private string[] textLines;
+    [SerializeField] private TextAsset TextFile;
+
+    void Start()
+    {
+        if(TextFile != null)
+        {
+            textLines = (TextFile.text.Split('\n'));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+}
