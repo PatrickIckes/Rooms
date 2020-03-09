349f6f9f993bf2bcb622ecf72c10344b3089e27b
jamespost <32137126+jamespost@users.noreply.github.com>
2020-03-09 11:52:22 -0500

    updated FMODLifetimeHandler

--- RoomsProject/Assets/Scripts/Audio/FMODLifetimeHandler.cs
+++ RoomsProject/Assets/Scripts/Audio/FMODLifetimeHandler.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class FMODLifetimeHandler : MonoBehaviour
 {
     /*Manages the lifetime of FMOD Studio objects*/
+    //has a camera gameobject
+    GameObject mainCamera;
 
     private void Start()
     {
+        //initialzie attributes
+        mainCamera = GameObject.Find("Main Camera");
+        //run FMODPersistence method
         FMODPersistence();
     }
 
     //Finds FMOD studio listener component and allows it to persist throughout scene changes
     private void FMODPersistence()
     {
-        DontDestroyOnLoad(GameObject.Find("Main Camera"));
+        //makes sure that the mainCamera is NOT destroyed during scene loading/changes so that audio can continue playback without interruption
+        DontDestroyOnLoad(mainCamera);
+        //check to see if mainCamera already has an FMOD studio listener and add one if it does not
+        if (!mainCamera.GetComponentInChildren<FMODUnity.StudioListener>())
+        {
+            mainCamera.AddComponent<FMODUnity.StudioListener>();
+        }
     }
 }
