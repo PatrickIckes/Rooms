9e6acf042564c9c8f9448829d0ce27ed33b318d4
jamespost <32137126+jamespost@users.noreply.github.com>
2020-03-10 12:20:30 -0500

    added sloth throw trash sfx in game

--- RoomsProject/Assets/Scripts/Audio/SlothSFX.cs
+++ RoomsProject/Assets/Scripts/Audio/SlothSFX.cs
@@ -27,4 +27,9 @@ public class SlothSFX : MonoBehaviour
     {
         FMODUnity.RuntimeManager.PlayOneShot("event:/NPC/Sloth/sx_game_npc_sloth_rummage_under_belly", GetComponent<Transform>().position);
     }
+    //jpost Audio
+    public void PlaySlothThrowTrash()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/NPC/Sloth/sx_game_npc_sloth_throw_trash", GetComponent<Transform>().position);
+    }
 }
