f0e96b8c6e2ccf19fb4b84748879d84037ab4446
jamespost <32137126+jamespost@users.noreply.github.com>
2020-03-10 11:38:13 -0500

    added sloth breathing sfx to in game animation

---
+++ RoomsProject/Assets/Scripts/Audio/SlothSFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlothSFX : MonoBehaviour
+{
+    ///jpost Audio
+    ///a class to handle Sloth's sfx
+
+    //jpost Audio
+    public void PlaySlothBreathInhale()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/NPC/Sloth/sx_game_npc_sloth_breathing_inhale", GetComponent<Transform>().position);
+    }
+    //jpost Audio
+    public void PlaySlothBreathExhale()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/NPC/Sloth/sx_game_npc_sloth_breathing_exhale", GetComponent<Transform>().position);
+    }
+}
