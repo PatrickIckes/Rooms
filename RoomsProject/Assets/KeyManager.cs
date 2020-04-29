a4fe8d41edbc9482fe70d9686d57eaa3617ee922
Detroit Blood <lboo300@gmail.com>
2020-04-29 16:20:13 -0500

    Figured out I'm a degenerate. Keys should work now, I made an error throw if things weren't a inventory.

--- RoomsProject/Assets/KeyManager.cs
+++ RoomsProject/Assets/KeyManager.cs
@@ -15,9 +15,9 @@ public class KeyManager : MonoBehaviour
         {
             key.SetActive(false);
         }
-        if ((playerInventory.CheckObject("EnvyMask") || playerInventory.CheckObject("EnvyMask(Clone)")) && (!playerInventory.CheckObject("PrideKey") || !playerInventory.CheckObject("PrideKey(Clone)"))) 
+        if ((playerInventory.CheckObject("EnvyMask") || playerInventory.CheckObject("EnvyMask")) && (!playerInventory.CheckObject("PrideKey") || !playerInventory.CheckObject("PrideKey(Clone)"))) 
             keys[2].SetActive(true);
-        else if ((playerInventory.CheckObject("SlothReward") || playerInventory.CheckObject("SlothReward(Clone)")) && (!playerInventory.CheckObject("EnvyKey") || !playerInventory.CheckObject("EnvyKey(Clone)"))) 
+        else if (playerInventory.CheckObject("SlothReward") && (!playerInventory.CheckObject("EnvyKey") || !playerInventory.CheckObject("EnvyKey(Clone)"))) 
             keys[1].SetActive(true);
         else if(!playerInventory.CheckObject("SlothKey") || !playerInventory.CheckObject("SlothKey(Clone)"))
             keys[0].SetActive(true);
