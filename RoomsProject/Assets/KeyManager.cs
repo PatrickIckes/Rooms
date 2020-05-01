b5732a5f72eb784ad9073d35f7fc890a0a83f633
Detroit Blood <lboo300@gmail.com>
2020-05-01 10:46:17 -0500

    Fixed key code to be a bit more clean

--- RoomsProject/Assets/KeyManager.cs
+++ RoomsProject/Assets/KeyManager.cs
@@ -16,27 +16,28 @@ public class KeyManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        foreach(GameObject key in keys)
+        foreach (GameObject key in keys)
         {
             key.SetActive(false);
         }
         Garbagebags.SetActive(true);
         stairs.SetActive(false);
-        if ((playerInventory.CheckObject("EnvyMask") || playerInventory.CheckObject("EnvyMask")) && (!playerInventory.CheckObject("PrideKey") || !playerInventory.CheckObject("PrideKey(Clone)")))
+        if (!playerInventory.CheckObject("SlothKey"))
+            keys[0].SetActive(true); 
+        else if (playerInventory.CheckObject("SlothReward") && !playerInventory.CheckObject("EnvyKey"))
+            keys[1].SetActive(true);
+        else if (playerInventory.CheckObject("EnvyMask") || playerInventory.CheckObject("PrideReward"))
         {
             Garbagebags.SetActive(false);
             stairs.SetActive(true);
-            keys[2].SetActive(true);
+            if ((!playerInventory.CheckObject("PrideKey") || !playerInventory.CheckObject("PrideKey(Clone)")))
+                keys[2].SetActive(true);
         }
-        else if (playerInventory.CheckObject("SlothReward") && (!playerInventory.CheckObject("EnvyKey") || !playerInventory.CheckObject("EnvyKey(Clone)")))
-            keys[1].SetActive(true);
-        else if (!playerInventory.CheckObject("SlothKey") || !playerInventory.CheckObject("SlothKey(Clone)"))
-            keys[0].SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
