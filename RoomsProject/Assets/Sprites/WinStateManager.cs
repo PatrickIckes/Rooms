0baa796fa049e59925645eea1e56cdcf643d6d6e
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2020-05-14 22:45:58 -0500

    Add win state scene.

---
+++ RoomsProject/Assets/Sprites/WinStateManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinStateManager : MonoBehaviour
+{
+    public Button MainMenuButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (MainMenuButton != null) MainMenuButton.onClick.AddListener(MainMenu);
+    }
+
+    void MainMenu()
+    {
+        //jpost Audio
+        PlayUIClickPlay();
+
+        SceneManager.LoadScene((int)Scenes.MainMenu);
+    }
+
+    //jpost audio
+    void PlayUIClickPlay()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/UI/sx_game_menu_ui_click_play", gameObject.transform.position);
+    }
+}
